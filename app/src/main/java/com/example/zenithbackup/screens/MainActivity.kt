979be0725a95e1c37e058dd50ed344395e0abc27
clencyc <clency2023@gmail.com>
2024-07-02 17:30:46 +0300

    Added the progress page

--- app/src/main/java/com/example/zenithbackup/screens/MainActivity.kt
+++ app/src/main/java/com/example/zenithbackup/screens/MainActivity.kt
@@ -1,47 +1,32 @@
 package com.example.zenithbackup.screens
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.zenithbackup.ui.theme.ZenithBackupTheme
+import com.example.zenithbackup.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            ZenithBackupTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        navView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> intent = Intent(this, HomeActivity::class.java)
+
+                R.id.profile -> intent = Intent(this, ProfileActivity::class.java)
+                R.id.progress -> intent = Intent(this, ProgressActivity::class.java)
             }
+            if (intent != null) {
+                startActivity(intent)
+            }
+            true
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ZenithBackupTheme {
-        Greeting("Android")
     }
 }
