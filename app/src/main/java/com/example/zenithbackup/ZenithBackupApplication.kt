8100841e0e55ad70054e883f99ee01c7b549c5bd
clencyc <clency2023@gmail.com>
2024-07-10 09:35:51 +0300

    Launcher activity for firebase

--- app/src/main/java/com/example/zenithbackup/ZenithBackupApplication.kt
+++ app/src/main/java/com/example/zenithbackup/ZenithBackupApplication.kt
@@ -6,6 +6,7 @@ import com.google.firebase.FirebaseApp
 class ZenithBackupApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        // this is the launcher activity
         FirebaseApp.initializeApp(this)
     }
 
